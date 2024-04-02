f674abb23a22a36e9ae9ca113018629b636c51dc
RuanVSouza <ruan2014souza17@gmail.com>
2024-04-02 14:30:34 -0300

    implementando IPersonService

---
+++ APIRest-ASP/APIRest-ASP/Services/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using APIRest_ASP.Model;
+
+namespace APIRest_ASP.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
---
+++ APIRest-ASP/APIRest-ASP/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,68 @@
+﻿using APIRest_ASP.Model;
+
+namespace APIRest_ASP.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+          
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> persons = new List<Person>();
+            for(int i = 0; i < 8; i++)
+            {
+                Person person = MockPerson(i);
+                persons.Add(person);
+            }
+            return persons;
+        }
+
+      
+
+        public Person FindById(long id)
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Ruan",
+                LastName = "Souza",
+                Address = "Rua A",
+                Gender = "Male"
+            };
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+        
+        
+        private Person MockPerson(int i)
+        {
+
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person name" + i,
+                LastName = "Person last name" + i,
+                Address = "Some address" + i,
+                Gender = "Male" + i
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+           return Interlocked.Increment(ref count);
+        }
+    }
+}
