19d300f12420574a582ed6f2bbb0f83ac19972b5
RuanVSouza <ruan2014souza17@gmail.com>
2024-04-02 14:52:07 -0300

    criado CRUD

--- APIRest-ASP/APIRest-ASP/Program.cs
+++ APIRest-ASP/APIRest-ASP/Program.cs
@@ -1,8 +1,13 @@
+using APIRest_ASP.Services;
+using APIRest_ASP.Services.Implementations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+//Injeção de dependencia
+builder.Services.AddScoped<IPersonService, PersonServiceImplementation>();
 
 var app = builder.Build();
 
